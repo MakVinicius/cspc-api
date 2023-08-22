4420daad9da830cd8c53b50f803bb600a8a4d347
Mak Vinícius <mvgj369@gmail.com>
2023-08-21 22:04:58 -0300

    Starting point Flyway

--- src/main/resources/db/migration/V1__Create_Initial_Structure.sql
+++ src/main/resources/db/migration/V1__Create_Initial_Structure.sql
@@ -1,7 +1,8 @@
 -- Create Roles table
 CREATE TABLE roles (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    CONSTRAINT pk_roles PRIMARY KEY (id)
 );
 
 -- Insert roles
@@ -11,93 +12,102 @@ INSERT INTO roles (name) VALUES ("ROLE_COORDINATOR");
 INSERT INTO roles (name) VALUES ("ROLE_SCRUM_MASTER");
 INSERT INTO roles (name) VALUES ("ROLE_INSTRUCTOR");
 
-
 -- Create Users table
 CREATE TABLE users (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(50),
-    lastName VARCHAR(50),
-    email VARCHAR(100),
-    password VARCHAR(255)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_users PRIMARY KEY (id)
 );
+
+ALTER TABLE users ADD CONSTRAINT uc_users_email UNIQUE (email);
 
 -- Create Users_Roles table
-CREATE TABLE users_roles(
-    user_id BIGINT,
-    role_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (role_id) REFERENCES roles(id)
+CREATE TABLE users_roles (
+    role_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL
 );
 
+ALTER TABLE users_roles ADD CONSTRAINT fk_userol_on_role FOREIGN KEY (role_id) REFERENCES roles (id);
+ALTER TABLE users_roles ADD CONSTRAINT fk_userol_on_user FOREIGN KEY (user_id) REFERENCES users (id);
 
 -- Create Coordinators table
 CREATE TABLE coordinators (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    user_id BIGINT NULL,
+    CONSTRAINT pk_coordinators PRIMARY KEY (id)
 );
 
+ALTER TABLE coordinators ADD CONSTRAINT FK_COORDINATORS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
 -- Create Classrooms table
 CREATE TABLE classrooms (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(100),
-    coordinator_id BIGINT,
-    FOREIGN KEY (coordinator_id) REFERENCES coordinators(id)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    coordinator_id BIGINT NOT NULL,
+    CONSTRAINT pk_classrooms PRIMARY KEY (id)
 );
 
+ALTER TABLE classrooms ADD CONSTRAINT FK_CLASSROOMS_ON_COORDINATOR FOREIGN KEY (coordinator_id) REFERENCES coordinators (id);
 
 -- Create Squads table
 CREATE TABLE squads (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    classroom_id BIGINT,
-    FOREIGN KEY (classroom_id) REFERENCES classrooms(id)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    classroom_id BIGINT NULL,
+    CONSTRAINT pk_squads PRIMARY KEY (id)
 );
 
+ALTER TABLE squads ADD CONSTRAINT FK_SQUADS_ON_CLASSROOM FOREIGN KEY (classroom_id) REFERENCES classrooms (id);
 
 -- Create Grades table
 CREATE TABLE grades (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    communication DECIMAL(2, 2),
-    collaboration DECIMAL(2, 2),
-    autonomy DECIMAL(2, 2),
-    quiz DECIMAL(2, 2),
-    individualChallenge DECIMAL(2, 2),
-    squadChallenge DECIMAL(2, 2),
-    finalGrade DECIMAL(2, 2)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    communication DOUBLE NOT NULL,
+    collaboration DOUBLE NOT NULL,
+    autonomy DOUBLE NOT NULL,
+    quiz DOUBLE NOT NULL,
+    individual_challenge DOUBLE NOT NULL,
+    squad_challenge DOUBLE NOT NULL,
+    final_grade DOUBLE NOT NULL,
+    CONSTRAINT pk_grades PRIMARY KEY (id)
 );
-
 
 -- Create Students table
 CREATE TABLE students (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT,
-    grade_id BIGINT,
-    squad_id BIGINT,
-    classroom_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (grade_id) REFERENCES grades(id),
-    FOREIGN KEY (squad_id) REFERENCES squads(id),
-    FOREIGN KEY (classroom_id) REFERENCES classrooms(id)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    grades_id BIGINT NULL,
+    user_id BIGINT NOT NULL,
+    squad_id BIGINT NULL,
+    classroom_id BIGINT NULL,
+    CONSTRAINT pk_students PRIMARY KEY (id)
 );
 
+ALTER TABLE students ADD CONSTRAINT FK_STUDENTS_ON_CLASSROOM FOREIGN KEY (classroom_id) REFERENCES classrooms (id);
+ALTER TABLE students ADD CONSTRAINT FK_STUDENTS_ON_GRADES FOREIGN KEY (grades_id) REFERENCES grades (id);
+ALTER TABLE students ADD CONSTRAINT FK_STUDENTS_ON_SQUAD FOREIGN KEY (squad_id) REFERENCES squads (id);
+ALTER TABLE students ADD CONSTRAINT FK_STUDENTS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
 -- Create Instructors table
 CREATE TABLE instructors (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT,
-    classroom_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (classroom_id) REFERENCES classrooms(id)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    user_id BIGINT NOT NULL,
+    classroom_id BIGINT NULL,
+    CONSTRAINT pk_instructors PRIMARY KEY (id)
 );
 
+ALTER TABLE instructors ADD CONSTRAINT FK_INSTRUCTORS_ON_CLASSROOM FOREIGN KEY (classroom_id) REFERENCES classrooms (id);
+ALTER TABLE instructors ADD CONSTRAINT FK_INSTRUCTORS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
 
 -- Create Scrum Masters table
 CREATE TABLE scrum_masters (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT,
-    classroom_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (classroom_id) REFERENCES classrooms(id)
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    user_id BIGINT NOT NULL,
+    classroom_id BIGINT NULL,
+    CONSTRAINT pk_scrum_masters PRIMARY KEY (id)
 );
+
+ALTER TABLE scrum_masters ADD CONSTRAINT FK_SCRUM_MASTERS_ON_CLASSROOM FOREIGN KEY (classroom_id) REFERENCES classrooms (id);
+ALTER TABLE scrum_masters ADD CONSTRAINT FK_SCRUM_MASTERS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
